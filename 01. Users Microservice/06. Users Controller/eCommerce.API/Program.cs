2249225022b931a1629d3f7ef61baed1f62f1a04
Ambarish Kumar Sharma <ambrishsha@gmail.com>
2024-11-26 15:01:55 +0530

    1. Added -> .AddJsonOptions     (         Options => {             Options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); //Converts string values to correspondent enums, like in our case Gender is enum type         }     );

--- 01. Users Microservice/06. Users Controller/eCommerce.API/Program.cs
+++ 01. Users Microservice/06. Users Controller/eCommerce.API/Program.cs
@@ -1,6 +1,8 @@
 using eCommerce.Infrastructure;
 using eCommerce.Core;
 using eCommerce.API.Middlewares;
+using Microsoft.Extensions.Options;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,12 @@ builder.Services.AddInfrastructure();
 builder.Services.AddCore();
 
 // Add controllers to the service collection
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions
+    (
+        Options => { 
+            Options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); //Converts string values to correspondent enums, like in our case Gender is enum type
+        }
+    );
 
 //Build the web application
 var app = builder.Build();
